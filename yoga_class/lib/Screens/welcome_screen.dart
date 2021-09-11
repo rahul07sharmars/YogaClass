4d191bbfaa8a417e716c71c4303814b3f2e0107d
rahul07sharmars <58891614+rahul07sharmars@users.noreply.github.com>
2021-09-11 08:55:15 +0530

    done

---
+++ yoga_class/lib/Screens/welcome_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  static String id = 'welcome_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
