c76007f5852d2f5a93c9ab98c8448bec786cdaf5
rahul07sharmars <58891614+rahul07sharmars@users.noreply.github.com>
2021-09-11 09:01:03 +0530

    Welcome_rough_done_figure

--- yoga_class/lib/Screens/welcome_screen.dart
+++ yoga_class/lib/Screens/welcome_screen.dart
@@ -6,7 +6,27 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('Welcome'),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextButton(onPressed: (){}, child: Text('Log In'),),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Log In'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
